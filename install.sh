f404b51c04ba6bfe8475c61d3ed47486c836d18e
Faroogh Karimi Zadeh <farooghkarimizadeh@gmail.com>
2016-03-11 15:36:54 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "WARNING! Installing this version will replace previous version of BHA."
 read -p "Remove or Install(r|i)?" choice
 if [ $choice = "i" ]; then
     cp ./bha /usr/bin/;
