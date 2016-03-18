dc16001ebd4ad6024c56951a0a7b00016e10cb23
Faroogh Karimi Zadeh <farooghkarimizadeh@gmail.com>
2016-03-18 14:13:41 +0330

    Delete install.sh

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "WARNING! Installing this version will replace previous version of BHA."
-read -p "Remove or Install(r|i)?" choice
-if [ $choice = "i" ]; then
-    cp ./bha /usr/bin/;
-    chmod +x /usr/bin/bha;
-elif [ $choice = "r" ]; then
-    rm /usr/bin/bha;
-fi
